3ece349c90640ced9f23c708c6ebfe361f73626a
Kunal Upadhya <kunalupadhya7@gmail.com>
2026-08-01 00:06:46 +0530

    Time: 5 ms (95.06%) | Memory: 48.2 MB (80.01%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,52 +1,31 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int max = nums[0];
-        int maxIdx = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = n - 2; i >= 0; i--) {
+            stack.push(nums[i]);
+        }
 
-        for (int i = 1; i < n; i++) { //it give me the first idx of maxElement
-            if (max < nums[i]) {
-                max = nums[i]; // why to find this because i know no element is greater than it so 
-                maxIdx = i; //  the ans id -1 for this 
+        int [] ans = new int[n];
+
+        for(int i =n-1;i>=0;i--){
+
+            while(!stack.isEmpty() && stack.peek()<=nums[i]){
+                stack.pop();
             }
+
+            if(stack.isEmpty()){
+                ans[i]=-1;
+            }
+
+            else{
+                ans[i]=stack.peek();
+            }
+
+            stack.push(nums[i]);
 
         }
 
-        Deque<Integer> st = new ArrayDeque<>();
-        st.push(max);
-
-        int ans[] = new int[n];
-        ans[maxIdx] = -1;
-
-        int i = maxIdx - 1;
-
-        if (i < 0){
-            i = n - 1;
-        }
-
-        while (i != maxIdx) {
-
-            while (!st.isEmpty() && st.peek() <= nums[i]) {
-                st.pop();
-            }
-
-            if (st.isEmpty()) {
-                ans[i] = -1;
-            }
-
-            else {
-                ans[i] = st.peek();
-            }
-
-            st.push(nums[i]);
-
-            i--;
-
-            if (i < 0){
-                i = n - 1;
-            }
-
-        }
         return ans;
 
     }
