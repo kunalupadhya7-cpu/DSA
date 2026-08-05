2a5efab675cdd821f32f908d474b27fe4f223484
Kunal Upadhya <kunalupadhya7@gmail.com>
2026-08-05 18:11:05 +0530

    Time: 0 ms (100.00%) | Memory: 80.4 MB (40.95%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,43 +1,32 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int n = arr.length;
-
         int low = 0;
         int high = n - 1;
-        int ans = -1;
+        int peek = -1;
 
         while (low <= high) {
 
             int guess = (low + high) / 2;
 
-            if (arr[guess] > arr[guess + 1] && arr[guess] > arr[guess - 1]) {
-
-                return guess;
-            }
-
-            else if (guess == 0) { // edge case  // when we are at '0' we cant do guess-1 but we know '0' cant be peek so we move right
+            if (guess == 0)
                 low = guess + 1;
-            }
-
-            else if (guess == n - 1) { // edge case // when we are at 'n-1' we cant do guess+1 but we know 'n-1' cant be peek so we move left
+            if (guess == n - 1)
                 high = guess - 1;
 
-            } // the 2 edge cases dont req comparission because we are sure where to move ...knowing starting and ending cant be peek 
-
-            else if (arr[guess] < arr[guess + 1] && arr[guess] > arr[guess - 1]) {
-                low = guess + 1; // right side approching
-                                 //guess+1 dont exist when guess=len-1 
+            if (arr[guess] > arr[guess + 1]) {
+                peek = guess;
+                high = guess - 1;
 
             }
 
-            else if (arr[guess] > arr[guess + 1] && arr[guess] < arr[guess - 1]) {
-                high = guess - 1; //left side approching
-                                  // guess-1 dont exist when guess=0
+            else {
+                low = guess + 1;
             }
 
         }
 
-        return ans;
+        return peek;
 
     }
 }
