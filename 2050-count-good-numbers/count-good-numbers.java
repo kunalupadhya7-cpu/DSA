b07d10601224874e53e740ee1faae59e47815c44
Kunal Upadhya <kunalupadhya7@gmail.com>
2026-09-05 19:13:51 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (87.02%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countGoodNumbers(long n) {
+        long o; // no of  odd pos
+        long e; // no of  even pos 
+        long mod =1000000007;
+
+        if (n % 2 == 0) {
+            o = n / 2;
+            e = n / 2;
+        } else {
+            o = n / 2;
+            e= (n / 2) + 1;
+        }
+       return (int)((calculatePower(5, e) * calculatePower(4, o)) % mod);
+       
+    }
+
+    public long calculatePower(int x, long power){
+         long mod =1000000007;
+
+        if (power==0) return 1;
+
+        long half = calculatePower(x,power/2);
+
+        if(power%2==0){
+            return( half*half % mod)  ;
+        }
+        else{
+            return(( (half*half) %mod) *x) %mod;
+        }
+
+
+    }
+}
