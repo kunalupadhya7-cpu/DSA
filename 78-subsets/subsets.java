c56c79bb9e852041c23914abe8c2ae539818ffcd
Kunal Upadhya <kunalupadhya7@gmail.com>
2026-09-09 12:46:58 +0530

    Time: 1 ms (86.97%) | Memory: 44.2 MB (56.58%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,32 @@
+class Solution {
+    List<List<Integer>> ans = new  ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+    
+      f(0,nums,new ArrayList<>());
+      return ans;
+        
+    }
+    public void  f(int idx, int[] arr,ArrayList<Integer> list){
+
+        if(idx==arr.length){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        //take
+        
+        list.add(arr[idx]);
+        f(idx+1,arr,list);
+
+        //backtrack
+
+        list.remove(list.size()-1);
+
+        //not take
+
+        f(idx+1,arr,list);
+
+    }
+                 
+    
+} // same as strivver pattern q ( one of the three templates he thaught )
