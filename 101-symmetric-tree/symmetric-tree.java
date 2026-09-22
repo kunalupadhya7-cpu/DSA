8a00ab91cb9215947d15daab0be95d6f33bc69ad
Kunal Upadhya <kunalupadhya7@gmail.com>
2026-09-22 21:50:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (52.38%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -1,58 +1,30 @@
-
 class Solution {
     public boolean isSymmetric(TreeNode root) {
 
-        if (root == null)
-            return true;
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while (!q.isEmpty()) {
-
-            int levelSize = q.size();
-            List<Integer> temp = new ArrayList<>();
-
-            while (levelSize != 0) {
-
-                TreeNode node = q.poll();
-
-                if (node == null) {
-                    temp.add(null);
-                    levelSize--;
-                    continue;
-                }
-
-                temp.add(node.val);
-
-                q.offer(node.left);
-                q.offer(node.right);
-
-                levelSize--;
-            }
-
-            // Every level must be a palindrome
-            if (!isPalindrome(temp))
-                return false;
-        }
-
-        return true;
+        if(root==null) return true;
+        
+        return f(root.left,root.right);
+        
     }
 
-    public boolean isPalindrome(List<Integer> list) {
+       public boolean f(TreeNode a, TreeNode b) {
 
-        int left = 0;
-        int right = list.size() - 1;
+        // Both null → same
+        if (a == null && b == null)
+            return true;
 
-        while (left <= right) {
+        // One null → different
+        if (a == null || b == null)
+            return false;
 
-            if (!Objects.equals(list.get(left), list.get(right)))
-                return false;
+        // Values different → different
+        if (a.val != b.val)
+            return false;
 
-            left++;
-            right--;
-        }
+       
+        boolean l = f(a.left, b.right);
+        boolean r = f(a.right, b.left);
 
-        return true;
+        return l && r;
     }
 }
