976670f7f54de86d59f1afba5c32b5a609040239
Kunal Upadhya <kunalupadhya7@gmail.com>
2026-09-23 16:08:26 +0530

    Time: 4 ms (19.71%) | Memory: 46.7 MB (16.95%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -1,46 +1,51 @@
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
 
-        if (subRoot == null) return true;
-        if (root == null) return false;
+        if (root == null && subRoot == null)
+            return true;
+        if (root == null || subRoot == null)
+            return false;
 
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
 
         while (!q.isEmpty()) {
+            int levelSize = q.size();
 
-            TreeNode t = q.poll();
+            while (levelSize != 0) {
 
-            if (t.val == subRoot.val && check(t, subRoot))
-                return true;
+                TreeNode t = q.poll();
 
-            if (t.left != null)
-                q.offer(t.left);
+                if (check(t, subRoot))
+                    return true;
 
-            if (t.right != null)
-                q.offer(t.right);
+                if (t.left != null)
+                    q.offer(t.left);
+                if (t.right != null)
+                    q.offer(t.right);
+
+                levelSize--;
+
+            }
+
         }
 
         return false;
+
+
     }
 
-    public boolean check(TreeNode a, TreeNode b) {
+    
 
-        // Both are null → same
-        if (a == null && b == null)
-            return true;
+    public boolean check(TreeNode a,TreeNode b){
+       if(a== null && b==null) return true;
+       if(a==null || b==null) return false;
+       if(a.val!=b.val) return false;
 
-        // One is null → different
-        if (a == null || b == null)
-            return false;
-
-        // Values different → different
-        if (a.val != b.val)
-            return false;
-
-        boolean l = check(a.left, b.left);
-        boolean r = check(a.right, b.right);
+        boolean l= check(a.left,b.left);
+        boolean r= check(a.right,b.right);
 
         return l && r;
+
     }
 }
