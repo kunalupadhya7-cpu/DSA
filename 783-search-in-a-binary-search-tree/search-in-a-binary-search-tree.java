09b69594e0d30a9571793200c4f4cea2e7734426
Kunal Upadhya <kunalupadhya7@gmail.com>
2026-09-25 17:33:52 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (35.10%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -1,22 +1,27 @@
+// this is og code time O(log(N)) and is simliar to binary search
+
+
 class Solution {
-
+    TreeNode ans =null;
     public TreeNode searchBST(TreeNode root, int val) {
-
-       
-        if (root == null)  return null;
-
-        if (root.val == val)  return root;
-
-        TreeNode l = searchBST(root.left, val);
-        if(l!=null) return l;
-
-        TreeNode r = searchBST(root.right, val);
-        if(r!=null) return r;
-
-        return null;
-
+        dfs (root,val);
+        return ans;
+        
     }
 
- 
+    public void dfs(TreeNode node,int val){
+        
+        if(node==null) return ; // similar to while(left<=right)
 
-}
+        if(node.val==val){ // to a[mid]== val
+            ans=node;
+            return ;
+        }
+        if(node.val<val){         //l=m+1;
+            dfs(node.right,val);
+        } 
+        else{
+            dfs(node.left,val); //r=m-1;
+        }
+    }
+} 
